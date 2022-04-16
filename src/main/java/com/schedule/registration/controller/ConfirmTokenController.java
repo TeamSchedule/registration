bbdb96030a4bf9d21a9c6432e154edbd9bef23ca
Vsevolod <seva151201@gmail.com>
2022-04-16 21:12:49 +0700

    Add token validation

--- src/main/java/com/schedule/registration/controller/ConfirmTokenController.java
+++ src/main/java/com/schedule/registration/controller/ConfirmTokenController.java
@@ -6,13 +6,14 @@ import com.schedule.registration.service.token.GetTokenByIdService;
 import com.schedule.registration.validator.TokenExpiredConstraint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@Validated
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/token")
 public class ConfirmTokenController {
